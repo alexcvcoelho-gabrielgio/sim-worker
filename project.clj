8ce360d8006f899e2aab71d4e38f2ac412c9010d
gabrielgio <gabrielgio@localhost.localdomain>
2017-05-21 23:32:53 -0300

    Add simulation code base

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [org.clojure/core.async "0.3.442"]
+                 [clj-http "3.5.0"]
+                 [cheshire "5.7.1"]
+                 [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot sim-worker.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/sim_worker/core.clj
+++ src/sim_worker/core.clj
@@ -1,7 +1,68 @@
 (ns sim-worker.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [clojure.core.async :as a :refer [go thread]]
+            [clj-http.client :as client]
+            [clojure.data.json :as json])
+  (:import (java.util UUID)))
+
+(def session-url "http://localhost:3000/api/session")
+(def track-url "http://gabrielgio.com.br:3001/api/track")
+
+(defn uuid
+  "Get a new UUID"
+  []
+  (str (UUID/randomUUID)))
+
+(defn post-session [body]
+  (client/post session-url {:form-params  body
+                            :content-type :json}))
+
+(defn call-api
+  "Call tracker api to register sim's location"
+  [{:keys [loc vel gas-lvl session-id hid]}]
+  (client/post))
+
+(defn parse-json
+  "Reads information from a json file and parse it"
+  []
+  )
+
+(defn get-session
+  "Starts a session from API"
+  [{:keys [brand model hid]}]
+  (let [res (post-session {:brand brand
+                                :model model
+                                :hd-id hid})]
+    (:session-id (json/read-str (:body res) :key-fn keyword))))
+
+(defn run-sim
+  "Runs simulation"
+  [sims session hid]
+  (doseq [sim sims]
+    (call-api (assoc sim :session-id session :hid hid))
+    (Thread/sleep (:dalay sim))))
+
+(defn run
+  "Gathers informations, start simulation
+  and start consuming api."
+  []
+  (let [hid (uuid)
+        s (get-session (assoc {} :brand "sim 0001" :model "x01" :hid hid))]
+    (println s)
+    (run-sim (parse-json) s hid)))
+
+(defn start-threads
+  "Starts all threads"
+  [n]
+  (loop [v n]
+    (if (> v 1)
+      (do
+        (go (run))
+        (recur (dec v))))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Start simulation"
   [& args]
-  (println "Hello, World!"))
+  (let [{:keys [options]} (parse-opts args [["-n" "--n NUMBER" :parse-fn #(Integer. %)]])]
+    (start-threads (:n options))))
