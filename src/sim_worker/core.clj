2f7bd1c132c35d674533681c1d42f7cd05046486
gabrielgio <gabrielgio@localhost.localdomain>
2017-05-28 18:07:38 -0300

    Code I lost last time formated my pc

--- src/sim_worker/core.clj
+++ src/sim_worker/core.clj
@@ -6,7 +6,7 @@
             [clojure.data.json :as json])
   (:import (java.util UUID)))
 
-(def session-url "http://localhost:3000/api/session")
+(def session-url "http://gabrielgio.com.br:3000/api/session")
 (def track-url "http://gabrielgio.com.br:3001/api/track")
 
 (defn uuid
@@ -14,34 +14,46 @@
   []
   (str (UUID/randomUUID)))
 
-(defn post-session [body]
+(defn post-session
+  "Start a session"
+  [body]
   (client/post session-url {:form-params  body
                             :content-type :json}))
 
 (defn call-api
   "Call tracker api to register sim's location"
-  [{:keys [loc vel gas-lvl session-id hid]}]
-  (client/post))
+  [item]
+  (client/post track-url {:form-params  (dissoc item :delay)
+                          :content-type :json}))
+
+(defn get-randon-info []
+  {:lat (rand-int 30)
+   :long (rand-int 30)
+   :vel (rand-int 100)
+   :gas-lvl (rand-int 30)
+   :delay 1})
 
 (defn parse-json
   "Reads information from a json file and parse it"
-  []
-  )
+  [n]
+  (loop [x 0 ar []]
+    (if (< x n)
+      (recur (inc x) (conj ar (get-randon-info)))
+      ar)))
 
 (defn get-session
   "Starts a session from API"
   [{:keys [brand model hid]}]
   (let [res (post-session {:brand brand
-                                :model model
-                                :hd-id hid})]
+                           :model model
+                           :hd-id hid})]
     (:session-id (json/read-str (:body res) :key-fn keyword))))
 
 (defn run-sim
   "Runs simulation"
   [sims session hid]
   (doseq [sim sims]
-    (call-api (assoc sim :session-id session :hid hid))
-    (Thread/sleep (:dalay sim))))
+    (call-api (assoc sim :session-id session ))))
 
 (defn run
   "Gathers informations, start simulation
@@ -49,8 +61,7 @@
   []
   (let [hid (uuid)
         s (get-session (assoc {} :brand "sim 0001" :model "x01" :hid hid))]
-    (println s)
-    (run-sim (parse-json) s hid)))
+    (run-sim (parse-json 100) s hid)))
 
 (defn start-threads
   "Starts all threads"
